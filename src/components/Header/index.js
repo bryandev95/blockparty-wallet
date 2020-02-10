eb7eaf77d1c39e27aea5790fd7fc5f6887b1821f
coldflame426 <sacred3091122@gmail.com>
2020-02-10 14:32:43 +0200

    Add wallet information page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import { MdHelp } from 'react-icons/md';
 
 import style from './style.module.scss';
 
-const Header = () => {
-  return <div className={style.header}>BlockParty Wallet</div>;
+const Header = ({ onToggle }) => {
+  return (
+    <div className={style.header}>
+      BlockParty Wallet
+      <div className={style.info}>
+        <MdHelp onClick={onToggle} />
+      </div>
+    </div>
+  );
+};
+
+Header.propTypes = {
+  onToggle: PropTypes.func.isRequired
 };
 
 export default Header;
--- src/components/OnBoarding/index.js
+++ src/components/OnBoarding/index.js
@@ -1,42 +1,57 @@
 import React, { useState } from 'react';
 import cx from 'classnames';
+import PropTypes from 'prop-types';
 
 import { Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
+import Generate from '../Generate';
+import WalletInfo from './components/WalletInfo';
 
 import style from './style.module.scss';
 
 const GENERATE = 1;
 const IMPORT = 2;
 
-const OnBoarding = () => {
+const OnBoarding = ({ showInfo, onToggle }) => {
   const [activeTab, setActiveTab] = useState(GENERATE);
 
   return (
-    <div>
-      <Nav tabs className={style.container}>
+    <div className={style.container}>
+      <Nav tabs>
         <NavItem>
           <NavLink
             className={cx(style.menuItem, { [style.active]: activeTab === GENERATE })}
             onClick={() => setActiveTab(GENERATE)}
           >
-            <span>GENERATE</span>
+            GENERATE
           </NavLink>
         </NavItem>
+
         <NavItem>
           <NavLink
             className={cx(style.menuItem, { [style.active]: activeTab === IMPORT })}
             onClick={() => setActiveTab(IMPORT)}
           >
-            <span>IMPORT</span>
+            IMPORT
           </NavLink>
         </NavItem>
       </Nav>
+
       <TabContent activeTab={activeTab}>
-        <TabPane tabId={GENERATE}>GENERATE</TabPane>
+        <TabPane tabId={GENERATE}>
+          <Generate />
+        </TabPane>
+
         <TabPane tabId={IMPORT}>IMPORT</TabPane>
       </TabContent>
+
+      <WalletInfo showInfo={showInfo} onToggle={onToggle} />
     </div>
   );
+};
+
+OnBoarding.propTypes = {
+  showInfo: PropTypes.bool.isRequired,
+  onToggle: PropTypes.bool.isRequired
 };
 
 export default OnBoarding;
