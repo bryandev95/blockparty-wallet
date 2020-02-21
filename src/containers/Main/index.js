8373992267f1ae498bb31765e141acd4e41d1f16
﻿Bryan Lee <bryan.lee.engineer95@gmail.com>
2020-02-21 18:59:23 +0200

    Update balance tab loading spinner

--- src/containers/Main/index.js
+++ src/containers/Main/index.js
@@ -1,14 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-import { ClipLoader } from 'react-spinners';
 import { Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
 import Actions from './components/Actions';
 import Balance from './components/Balance';
 import Receive from './components/Receive';
-
-import { getTokenInfo } from 'contexts/utils';
 
 import style from './style.module.scss';
 
@@ -17,36 +14,8 @@ const BALANCE = 2;
 const SEND = 3;
 const RECEIVE = 4;
 
-const usePrevious = value => {
-  const ref = useRef();
-
-  useEffect(() => {
-    ref.current = value;
-  });
-
-  return ref.current;
-};
-
 const Main = ({ balances, wallet }) => {
-  const [isLoading, setLoading] = useState(false);
   const [activeTab, setActiveTab] = useState(ACTIONS);
-  const [tokens, setTokens] = useState([]);
-  const prevBalance = usePrevious(balances);
-
-  useEffect(() => {
-    if (!balances.tokens) return;
-
-    setLoading(true);
-    getTokenInfo(balances.tokens)
-      .then(response => {
-        setLoading(false);
-        setTokens(response);
-      })
-      .catch(() => {
-        setLoading(false);
-      });
-    // eslint-disable-next-line
-  }, [prevBalance]);
 
   return (
     <div className={style.container}>
@@ -91,19 +60,13 @@ const Main = ({ balances, wallet }) => {
             <Actions />
           </TabPane>
           <TabPane tabId={BALANCE}>
-            <Balance balances={balances} tokens={tokens} />
+            <Balance balances={balances} />
           </TabPane>
           <TabPane tabId={SEND}>SEND</TabPane>
           <TabPane tabId={RECEIVE}>
             <Receive wallet={wallet} />
           </TabPane>
         </TabContent>
-
-        {isLoading && activeTab === BALANCE && (
-          <div className={style.loaderContainer}>
-            <ClipLoader size="35px" />
-          </div>
-        )}
       </div>
     </div>
   );
